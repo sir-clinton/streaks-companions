3baf3807fcb9681a1728389e6159ab98d7c10b6e
calif <muneneclinton159@gmail.com>
2025-11-29 13:04:33 +0300

    cmmite

--- server.js
+++ server.js
@@ -1151,12 +1151,12 @@ app.get('/register', (req, res)=> {
 
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
+    host: "nairobiperv@gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+    user: process.env.EMAIL_USER,   // your Brevo account email
+    pass: process.env.BREVO_API_KEY // your Brevo SMTP key
   }
 });
 
